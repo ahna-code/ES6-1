e43807862e1c8c42064ac2d4bcb6ff7ef6ad4dbd
Agnieszka Korczak <korczak.a.m@gmail.com>
2018-12-11 22:40:59 +0100

    Fix index-4.js

--- index-4.js
+++ index-4.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
-const tab = [...grades];
-let average = (tab) => tab.reduce((a, b) => a + b) / tab.length;
-console.log(average(tab));
+
+let average = (...grades) => grades.reduce((a, b) => a + b) / grades.length;
+console.log(average(...grades));
 
